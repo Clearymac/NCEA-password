e0c58e6c665d4382a103f380778c6f69190584b5
Clearymac <72894742+Clearymac@users.noreply.github.com>
2021-08-06 14:21:23 +1200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,22 @@ bruh2 = ""
 bruh3 = ""
 bruh4 = ""
 bruh5 = ""
+bruh6 = ""
+bruh7 = ""
+bruh8 = ""
+bruh9 = ""
+bruh10 = ""
+bruh11 = ""
+bruh12 = ""
+bruh13 = ""
+bruh14 = ""
+bruh15 = ""
+bruh16 = ""
+bruh17 = ""
+bruh18 = ""
+bruh19 = ""
+bruh20 = ""
+
 
 for length in range(char_amount):
 
@@ -20,13 +36,14 @@ for length in range(char_amount):
     ran_char = random.randint(0,89)
 
     bruh = CHAR_LIST[ran_char]
+    
     if length == 0:
         bruh1 = (bruh)
 
     elif length == 1:
         bruh2 = (bruh)
 
-    if length == 2:
+    elif length == 2:
         bruh3 = (bruh)
 
     elif length == 3:
@@ -34,6 +51,33 @@ for length in range(char_amount):
 
     elif length == 4:
         bruh5 = (bruh)
+
+    elif length == 5:
+        bruh6 = (bruh)
+
+    elif length == 6:
+        bruh7 = (bruh)
+
+    elif length == 7:
+        bruh8 = (bruh)
+
+    elif length == 8:
+        bruh9 = (bruh)
+
+    elif length == 9:
+        bruh10 = (bruh)
+        
+    elif length == 10:
+        bruh11 = (bruh)
+        
+    elif length == 11:
+        bruh12 = (bruh)
+        
+    elif length == 12:
+        bruh13 = (bruh)
+        
+    elif length == 13:
+        bruh14 = (bruh)
 
 password = (bruh1 + bruh2 + bruh3 + bruh4 + bruh5)
 print(password)
