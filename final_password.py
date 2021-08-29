229753165ee0627e54da6f928818ba814d7f426b
GGPC <clearyd@stu.otc.school.nz>
2021-08-30 11:46:46 +1200

    fixed intro

--- final_password.py
+++ final_password.py
@@ -3,24 +3,25 @@ import random
 import csv
 
 # info on types of passwords
-regular_info = "'Regular' uses every main character on a standard US keyboard and randomly picks from any of these chars to make the password"
-numbers_only_info = "Only uses numbers in the password and randomly chooses the numbers to be added to the password"
-letter_only_info = "only use letter(upper and lower) in the password and randomly chooses the letter to be added to the password"
-special_char_info = "only uses special characters on the standard US keyboard for the password... e.g. !@#$%^&*()"
-uppercase_info = "similar to 'regular' but every letter will only be uppercase"
-lowercase_info = "similar to 'regular' but every letter will only be uppercase"
+regular_info = "'Regular' uses every main character on a standard US keyboard and randomly picks from any of these chars to make the password\n\n\n"
+numbers_only_info = "Only uses numbers in the password and randomly chooses the numbers to be added to the password\n\n\n"
+letter_only_info = "only use letter(upper and lower) in the password and randomly chooses the letter to be added to the password\n\n\n"
+special_char_info = "only uses special characters on the standard US keyboard for the password... e.g. !@#$%^&*()\n\n\n"
+uppercase_info = "similar to 'regular' but every letter will only be uppercase\n\n\n"
+lowercase_info = "similar to 'regular' but every letter will only be uppercase\n\n\n"
 
 
 bruh = True
 #intro user to the password generator and show options for type of password and possible info
 print("Password Generator: \n\n\n\n\n")
-while True:
+while bruh == True:
+
     print("""you have multiple options for the password you want generated...
     (1)regular ||| (2)Numbers only ||| (3) letters only ||| (4)Special Characters only ||| (5)Uppercase ||| (6)Lowercase
     For information on what these do type the number next to the password type you would like to learn about...
     if wanting to continue press 'e' """)
     # input for if user would like to learn about what a specific password type does
-    pswrd_info = int(input("..."))
+    pswrd_info = input("...")
 
     # displays password type info on user request
     if pswrd_info == "1":
